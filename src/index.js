9c24ad2f59757f4cee9d0b3aa4efe6e8a3d3a304
Sangeetha R <109620687+Sangee-R@users.noreply.github.com>
2023-07-02 13:48:43 +0000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Login from "./Login";
-import "./style1.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="g-signin">
-      <Login />
-    </div>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
